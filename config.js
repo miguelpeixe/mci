0975d2e93cce441333825d6fb0321c7030b8b15f
Miguel Peixe <miguel@cardume.art.br>
2014-07-14 15:30:29 -0300

    add procfile and default config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 require('dotenv').load();
 
 module.exports = {
-	apiUrl: process.env.MCI_API_URL,
-	projectId: process.env.MCI_PROJECT_ID
+	apiUrl: process.env.MCI_API_URL || 'http://mapasculturais.hacklab.com.br/api',
+	projectId: process.env.MCI_PROJECT_ID || 4
 }
